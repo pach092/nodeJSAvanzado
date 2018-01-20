6094f23f1bd2b5137e6e22e4ce0333223fbb8140
pach092 <pachobernal5@gmail.com>
2018-01-20 18:16:53 -0500

    Curso de Node avanzado

---
+++ platziverse-agent/utils.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function parsePayload (payload) {
+  if (payload instanceof Buffer) {
+    payload = payload.toString('utf8')
+  }
+
+  try {
+    payload = JSON.parse(payload)
+  } catch (e) {
+    payload = null
+  }
+}
--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -1,35 +1,44 @@
 'use strict'
 
-const db = require('./')
 const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
 const chalk = require('chalk')
+const db = require('./')
 
-asyncfunctionsetup () {
-  const value = process.argv.filter(arg => arg === '-y' || arg === '--yes')[0]
+const prompt = inquirer.createPromptModule()
 
-  if (value === '-y' || value === '--yes') {
-    const config = {
-      database: process.env.DB_NAME || 'platziverse',
-      username: process.env.DB_USER || 'platzi',
-      password: process.env.DB_PASS || 'platzi',
-      host: process.env.DB_HOST || 'localhost',
-      dialect: process.env.DB_USAGE || 'postgres',
-      setup: true,
-      logging: m => debug(m)
+async function setup() {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?'
     }
+  ])
 
-    await db(config).catch(handleFatalError)
-
-    console.log('Success!')
-    process.exit(0)
-  } else {
-    console.log(chalk.green(`La base de datos esta a salvo`))
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
   }
+
+  const config = {
+    database: process.env.DB_NAME || 'platziverse',
+    username: process.env.DB_USER || 'platzi',
+    password: process.env.DB_PASS || 'platzi',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: s => debug(s),
+    setup: true
+  }
+
+  await db(config).catch(handleFatalError)
+
+  console.log('Success!')
+  process.exit(0)
 }
 
-functionhandleFatalError (err) {
-  console.error(chalk.red(err.message))
-  console.error(chalk.red(err.stack))
+function handleFatalError(err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
   process.exit(1)
 }
 
