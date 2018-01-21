f5e7677b387bd193a22fc525fd1b018fd51338e9
pach092 <pachobernal5@gmail.com>
2018-01-20 20:56:13 -0500

    Front End app

---
+++ platziverse-web/config.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+    endpoint: process.env.API_ENDPOINT || 'http://localhost:3000',
+    apiToken: process.env.API_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBsYXR6aSIsImFkbWluIjoidHJ1ZSIsInBlcm1pc3Npb25zIjpbIm1ldHJpY3M6cmVhZCJdLCJpYXQiOjE1MTY0OTg1NTN9.Y4vti3gLgyrd1uDdy2qjTj8x8rjmxpBIVcZSWlxy_gA'
+}
